212902cef5fa7664710496aac894720ce4a451b1
Patricia Magalhães <paath.magalhaes2@gmail.com>
2025-07-07 20:01:33 -0300

    chore: create product create component and add navigation

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./views/home/home.component";
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
 
 export const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "products", component: ProductCrudComponent }
+  { path: "products", component: ProductCrudComponent },
+  { path: "products/create", component: ProductCreateComponent },
 ];
--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-crud',
-  imports: [],
-  templateUrl: './product-crud.component.html',
-  styleUrl: './product-crud.component.css'
+  selector: "app-product-crud",
+  imports: [MatButtonModule],
+  templateUrl: "./product-crud.component.html",
+  styleUrl: "./product-crud.component.css",
 })
 export class ProductCrudComponent {
-
+  constructor(private readonly router: Router) {}
+  navigateToProductCreate(): void {
+    this.router.navigate(["/products/create"]);
+  }
 }
