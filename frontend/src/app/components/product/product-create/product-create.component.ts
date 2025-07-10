1bc22bb7d4b86d05fd9d62556402e9be8246fb1f
Patricia Magalhães <paath.magalhaes2@gmail.com>
2025-07-09 22:13:21 -0300

    chore: create product model and product service

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Router } from "@angular/router";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
-  selector: 'app-product-create',
-  imports: [],
-  templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  selector: "app-product-create",
+  imports: [MatButtonModule],
+  templateUrl: "./product-create.component.html",
+  styleUrl: "./product-create.component.css",
 })
 export class ProductCreateComponent {
+  constructor(
+    private readonly productService: ProductService,
+    private readonly router: Router
+  ) {}
 
+  createProduct(): void {
+    this.productService.showMessage("Operação executada com sucesso!");
+  }
+
+  cancel(): void {
+    this.router.navigateByUrl("/products");
+  }
 }
