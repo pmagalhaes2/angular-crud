50c921c2ee3cde1d2127cd1553a25684f510b53d
Patricia Magalhães <paath.magalhaes2@gmail.com>
2025-07-16 11:46:14 -0300

    chore: use ngFor directive to list all products

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatListModule } from "@angular/material/list";
 import { Product } from "../product-create/product.model";
 import { ProductService } from "./../product.service";
 
 @Component({
   selector: "app-product-read",
-  imports: [],
+  imports: [MatCardModule, MatListModule, CommonModule],
   templateUrl: "./product-read.component.html",
   styleUrl: "./product-read.component.css",
 })
